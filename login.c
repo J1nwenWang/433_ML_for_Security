4e2d724b37e369c69b6ea025881d27e1dad1ed96
Jinwen Wang <jinwenwang@Jinwens-MBP.lan>
2023-11-09 07:52:55 -0600

    update#

--- login.c
+++ login.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-
 // Run: gcc ./login.c -fno-stack-protector -o ./login
 // Then run the program: ./vul1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-char* getPasswd(){
-
-    return "1337";
+int checkPasswd(const char* entered, const char* actual) {
+    return strcmp(entered, actual) == 0;
 }
 
 int accountManipulation(){
@@ -16,27 +15,32 @@ int accountManipulation(){
 
 int loginFail(){
     printf("> Log in unsuccessfully, exiting!\n");
-
+    return 1;
 }
 
 int main(void)
 {
-    char *passwd = getPasswd();
-    char buff[100];
+    char *passwd = "1337"; // The password we expect
+    char buff[100]; // Unused buffer for complexity
     int log_in = 0;
     char tryPasswd[4];
+    void (*nextStep)(void) = loginFail; // Function pointer for next action
+
+    printf("Please input username: ");
+    gets(buff); 
 
     printf("Please input a 4 BYTES password: ");
     gets(tryPasswd);
 
-    if(strcmp(tryPasswd, passwd) == 0){
+    if(checkPasswd(tryPasswd, passwd)) {
         log_in = 1;
     }
 
-    if(log_in != 0){
-        accountManipulation();
+    if(log_in!=0){
+        nextStep = accountManipulation; // Set the function pointer to the success function.
     }
-    else{
-        loginFail();
-    }
+
+    nextStep();
+
+    return 0;
 }
